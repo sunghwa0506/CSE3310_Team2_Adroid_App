0f995eb15d1e0d78830b8da3b0e74e057bf9ffb2
sunghwa0506 <71661335+sunghwa0506@users.noreply.github.com>
2020-11-08 15:16:08 -0600

    2nd commit

--- app/src/main/java/com/example/team2/MainActivity.java
+++ app/src/main/java/com/example/team2/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.team2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    if(FirebaseAuth.getInstance().getCurrentUser()==null){
+        startSignActivity();
+    }
+
+    findViewById(R.id.Logoutbutton).setOnClickListener(onclickListner);
+    }
+
+    View.OnClickListener onclickListner = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.Logoutbutton:
+                    FirebaseAuth.getInstance().signOut();
+                    startSignActivity();
+                    break;
+
+            }
+        }
+    };
+
+
+    private void startSignActivity(){
+        Intent intent = new Intent(this, SingupActivity.class);
+        startActivity(intent);
+    }
+    private void sport(){
+        Intent intent = new Intent(this, SportsTrainingFragment.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/team2/SingupActivity.java
+++ app/src/main/java/com/example/team2/SingupActivity.java
@@ -1,9 +1,10 @@
 package com.example.team2;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,7 +26,8 @@ public class SingupActivity extends AppCompatActivity {
 
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
-        findViewById(R.id.sign_up_button).setOnClickListener(onClickListener);
+        findViewById(R.id.loginbutton).setOnClickListener(onClickListener);
+        findViewById(R.id.gotploginbutton).setOnClickListener(onClickListener);
     }
 
     @Override
@@ -35,12 +37,30 @@ public class SingupActivity extends AppCompatActivity {
         FirebaseUser currentUser = mAuth.getCurrentUser();
     }
 
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        moveTaskToBack(true);
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(1);
+    }
+
+
+    private void startLoginActivity(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+
     View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             switch(v.getId()){
-                case R.id.sign_up_button:
+                case R.id.loginbutton:
                     signUp();
+                    break;
+                case R.id.gotploginbutton:
+                    startLoginActivity();
                     break;
                 default:
                     throw new IllegalStateException("Unexpected value: " + v.getId());
@@ -48,25 +68,42 @@ public class SingupActivity extends AppCompatActivity {
         }
     };
 
-    private void signUp(){
-        String email = ((EditText)findViewById(R.id.editTextTextEmailAddress)).getText().toString();
-        String password = ((EditText)findViewById(R.id.editTextTextPassword)).getText().toString();
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                        }
 
-                        // ...
-                    }
-                });
+    private void startToast(String msg){
+        Toast.makeText(this, msg,Toast.LENGTH_SHORT).show();
+    }
+
+
+    private void signUp(){
+        String email = ((EditText)findViewById(R.id.LoginID)).getText().toString();
+        String password = ((EditText)findViewById(R.id.LoginPW)).getText().toString();
+        String passwordCheck = ((EditText)findViewById(R.id.editTextTextPassword2)).getText().toString();
+        if(email.length()>0&&password.length()>0&&passwordCheck.length()>0){
+            if(password.equals(passwordCheck))
+            {
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    startToast( "Success");
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    if(task.getException()!=null){
+                                        startToast( task.getException().toString());
+                                    }
+                                }// ...
+                            }
+                        });
+            }
+            else { startToast( "Password not match"); } }
+        else{
+            startToast( "Please enter Email or Password");
+        }
+
+
 
     }
 
